a2ad2e1203a37928543d5f6ef7483590aa942996
Nikita <belka.nik02@gmail.com>
2024-09-05 21:30:29 +0300

    Дополнили информацию

--- src/pages/PaymentDetailsPage/PaymentDetailsPage.jsx
+++ src/pages/PaymentDetailsPage/PaymentDetailsPage.jsx
@@ -1,14 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 import { getPaymentsById } from "../../../payments-api";
 
 export default function PaymentDetailsPage() {
   const { paymentId } = useParams();
+  const [payment, setPayment] = useState(null);
 
   useEffect(() => {
     async function getData() {
       try {
         const data = await getPaymentsById(paymentId);
+        setPayment(data);
       } catch (error) {
         
       }
@@ -19,6 +21,15 @@ export default function PaymentDetailsPage() {
   return (
   <div>
     <h1>PaymentDetailsPage: {paymentId}</h1>
+
+    {payment && 
+    <div>
+      <p>Amount: {payment.amount}</p>
+      <p>Number: {payment.cardNumber}</p>
+      <p>Owner: {payment.cardOwner}</p>
+      <p>Type: {payment.cardType}</p>
+      <p>Description: {payment.description}</p>
+    </div>}
   </div>
   )
 }
