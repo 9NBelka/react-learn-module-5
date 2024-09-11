dfe17c0d4221c3523ee00818c6f4f327d8b2ecf0
Nikita <belka.nik02@gmail.com>
2024-09-12 00:13:52 +0300

    Update NotFoundPage.jsx

--- src/pages/NotFoundPage.jsx/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx/NotFoundPage.jsx
@@ -2,7 +2,7 @@ import {Link} from 'react-router-dom'
 export default function NotFoundPage() {
   return (
       <>
-      <div><h1>Opps! Not Found!!!</h1></div>
+      <div><h1>Opps! Not Found!!!404</h1></div>
       <Link to="/"><h4>Back to home page</h4></Link>
       </>
   )
