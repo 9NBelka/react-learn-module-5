db1e3f93eaaec5b043c8aac9725442cf1a53dad2
Nikita <belka.nik02@gmail.com>
2024-11-05 16:46:13 +0200

    Реализовал корзину и сделал перелинковку страниц

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,29 +1,27 @@
 // import { useState } from 'react'
 
-import { Route, Routes } from 'react-router-dom'
-import Navigation from '../Navigation/Navigation'
-import HomePage from '../../pages/HomePage/HomePage'
-import PaymentsPage from '../../pages/PaymentsPage/PaymentsPage'
-import NotFoundPage from '../../pages/NotFoundPage.jsx/NotFoundPage'
-import PaymentDetailsPage from '../../pages/PaymentDetailsPage/PaymentDetailsPage'
+import { Route, Routes } from 'react-router-dom';
+import Navigation from '../Navigation/Navigation';
+import HomePage from '../../pages/HomePage/HomePage';
+import PaymentsPage from '../../pages/PaymentsPage/PaymentsPage';
+import NotFoundPage from '../../pages/NotFoundPage.jsx/NotFoundPage';
+import PaymentDetailsPage from '../../pages/PaymentDetailsPage/PaymentDetailsPage';
 function App() {
-
-
-  return ( 
+  return (
     <>
       <Navigation />
 
       <Routes>
-        <Route path="/" element={<HomePage />}/>
-        <Route path="/payments" element={<PaymentsPage />}/>
-        <Route path="/payments/:paymentId" element={<PaymentDetailsPage />}>
-          <Route path='subpage-a' element={<div>Subpage A</div>}/>
-          <Route path='subpage-b' element={<div>Subpage B</div>}/>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/payments' element={<PaymentsPage />} />
+        <Route path='/payments/:paymentId' element={<PaymentDetailsPage />}>
+          <Route path='subpage-a' element={<div>Subpage A</div>} />
+          <Route path='subpage-b' element={<div>Subpage B</div>} />
         </Route>
-        <Route path="*" element={<NotFoundPage />}/>
+        <Route path='*' element={<NotFoundPage />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
